dbc9026699ea0fea23400414f4ee7455b7fe1ce8
alex Gainullin <againullin8@gmail.com>
2024-03-04 14:57:32 +0200

    Typing test

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,33 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
   const addNote = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
   }
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
+        {notes.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
       <form onSubmit={addNote}>
-        <input />
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
         <button type="submit">save</button>
-      </form>   
+      </form>
     </div>
   )
 
